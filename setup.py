07c27d07ed77e6cf664c07fa37e8a201a48095e5
Matt Dupre <matt@tigera.io>
2020-06-23 12:44:34 -0700

    WIP

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def get_install_requires():
     res.append('boto3>=1.9.142')
     res.append('requests_aws4auth>=0.9')
     res.append('click>=6.7,<7.0')
-    res.append('pyyaml==3.12')
+    res.append('pyyaml==5.3.1')
     res.append('voluptuous>=0.9.3')
     res.append('certifi>=2019.3.9')
     res.append('six>=1.11.0')
